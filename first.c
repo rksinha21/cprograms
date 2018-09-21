f4a1715f821a3668dc55d9edad0c7523a009d970
rksinha21 <43473971+rksinha21@users.noreply.github.com>
2018-09-21 19:36:18 +0530

    Adding indentation
    Changing the values of the variables from pre-assigned to user input.

--- first.c
+++ first.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int a=10,b=345,sum=0;
-sum=a+b;
-printf("Sum=%d",sum);
+  int a,b,sum=0;
+  printf("Enter two numbers");
+  scanf("%d%d",&a,&b); 
+  sum=a+b;
+  printf("Sum=%d",sum);
 }
